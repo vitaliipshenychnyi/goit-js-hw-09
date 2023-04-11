88cea9d1fed15b6b68e184c84f28deb747be106a
vitaliipshenychnyi <117928970+vitaliipshenychnyi@users.noreply.github.com>
2023-04-11 05:51:23 +0000

    Deploying to gh-pages from @ vitaliipshenychnyi/goit-js-hw-09@6a340b73ca512c84478a41b9f758e141daf21672 🚀

--- 03-promises.071905d4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form"),t=0;e.addEventListener("submit",(function(e){e.preventDefault(),t+=1;var n=e.currentTarget.elements.delay.value;e.currentTarget.elements.step.value,e.currentTarget.elements.amount.value;!function(e,t){Math.random()>.3?(console.log(e),console.log(t)):console.log(!1)}(t,n)}))}();
-//# sourceMappingURL=03-promises.071905d4.js.map
--- 03-promises.51421478.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form");let t=0;e.addEventListener("submit",(e=>{e.preventDefault(),t+=1;const n=e.currentTarget.elements.delay.value;e.currentTarget.elements.step.value,e.currentTarget.elements.amount.value;!function(e,t){Math.random()>.3?(console.log(e),console.log(t)):console.log(!1)}(t,n)}));
-//# sourceMappingURL=03-promises.51421478.js.map
